3cd2fc2ec502b6ffda84120e3f45d8502623669b
Oleksandr Mamchur <irex1988@gmail.com>
2024-04-19 19:33:00 +0300

    fixed tests, added 2 examples from instructions

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -8,13 +8,13 @@ from pagination import pagination
     [
         (1, 10, 2, 0, "1 2 ... 9 10"),
         (10, 10, 2, 0, "1 2 ... 9 10"),
-        (8, 10, 2, 0, "1 2 ... 8 9 10"),
-        (3, 10, 2, 0, "1 2 3 ... 9 10"),
+        (4, 5, 1, 0, "1 ... 4 5"),
+        (4, 10, 2, 2, "1 2 3 4 5 6 ... 9 10"),
         (5, 10, 2, 0, "1 2 ... 5 ... 9 10"),
         (100, 10, 2, 0, "1 2 ... 9 10"),
     ],
 )
-def test_pagination_different_current_page_total_pages(
+def test_pagination_different_current_page(
     current_page, total_pages, boundaries, around, expected_result
 ):
     assert (
@@ -26,10 +26,26 @@ def test_pagination_different_current_page_total_pages(
 @pytest.mark.parametrize(
     "current_page, total_pages, boundaries, around, expected_result",
     [
-        (1, 10, 1, 0, "1 ... 10"),
+        (5, 1, 1, 1, "1"),
+        (5, 10, 2, 1, "1 2 ... 4 5 6 ... 9 10"),
+        (5, 100000, 1, 1, "1 ... 4 5 6 ... 100000"),
+    ],
+)
+def test_pagination_different_total_pages(
+    current_page, total_pages, boundaries, around, expected_result
+):
+    assert (
+        pagination(current_page, total_pages, boundaries, around)
+        == expected_result
+    )
+
+
+@pytest.mark.parametrize(
+    "current_page, total_pages, boundaries, around, expected_result",
+    [
+        (1, 10, 0, 1, "1 2 ..."),
+        (3, 10, 0, 1, "... 2 3 4 ..."),
         (5, 10, 2, 0, "1 2 ... 5 ... 9 10"),
-        (3, 10, 2, 0, "1 2 3 ... 9 10"),
-        (8, 10, 2, 0, "1 2 ... 8 9 10"),
         (5, 10, 100, 0, "1 2 3 4 5 6 7 8 9 10"),
     ],
 )
@@ -48,7 +64,7 @@ def test_pagination_different_boundaries(
         (1, 10, 1, 2, "1 2 3 ... 10"),
         (10, 10, 1, 2, "1 ... 8 9 10"),
         (5, 10, 1, 2, "1 ... 3 4 5 6 7 ... 10"),
-        (5, 10, 1, 100, "1 2 3 4 5 6 7 8 9 10"),
+        (5, 10, 1, 0, "1 ... 5 ... 10"),
     ],
 )
 def test_pagination_different_around(
@@ -63,10 +79,11 @@ def test_pagination_different_around(
 @pytest.mark.parametrize(
     "current_page, total_pages, boundaries, around, expected_result",
     [
-        (1000000, 10, 2, 1, "1 2 ... 9 10"),
-        (3, 10, 1000000, 1, "1 2 3 4 5 6 7 8 9 10"),
-        (3, 10, 2, 1000000, "1 2 3 4 5 6 7 8 9 10"),
-        (3, 1000000, 2, 1, "1 2 3 4 ... 999999 1000000"),
+        (1000000000, 10, 2, 1, "1 2 ... 9 10"),
+        (3, 1000000000, 2, 1, "1 2 3 4 ... 999999999 1000000000"),
+        (3, 10, 1000000000, 1, "1 2 3 4 5 6 7 8 9 10"),
+        (3, 10, 2, 1000000000, "1 2 3 4 5 6 7 8 9 10"),
+
     ],
 )
 def test_pagination_big_values(
@@ -83,7 +100,7 @@ def test_pagination_big_values(
     [
         (0, 10, 2, 1),
         (4, 0, 2, 1),
-        (4, 10, 0, 1),
+        (4, 10, -1, 1),
         (4, 10, 2, -1),
     ],
 )
@@ -103,7 +120,7 @@ def test_pagination_raises_value_error(
         (4, 10, 2, [1, 3]),
     ],
 )
-def test_pagination_raises_value_error(
+def test_pagination_not_int_raises_value_error(
     current_page, total_pages, boundaries, around
 ):
     with pytest.raises(ValueError, match="All values must be integers"):
