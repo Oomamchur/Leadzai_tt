2aee58861e77b72102d3d92bdfe43872a857c081
Oleksandr Mamchur <irex1988@gmail.com>
2024-04-22 17:07:53 +0300

    fixed unexpected behavior

--- pagination.py
+++ pagination.py
@@ -2,14 +2,12 @@ def pagination(
     current_page: int, total_pages: int, boundaries: int, around: int
 ) -> str:
     if not all(
-        isinstance(variable, int)
-        for variable in [current_page, total_pages, boundaries, around]
+        isinstance(argument, int)
+        for argument in [current_page, total_pages, boundaries, around]
     ):
         raise ValueError("All values must be integers")
     if current_page < 1 or total_pages < 1:
-        raise ValueError(
-            "current_page & total_pages must be int, greater that 0"
-        )
+        raise ValueError("current_page & total_pages must be greater that 0")
     if around < 0 or boundaries < 0:
         raise ValueError("amount & boundaries must be positive int")
 
@@ -31,7 +29,6 @@ def pagination(
         if current_page + around > total_pages
         else current_page + around
     )
-
     current_around = [page for page in range(start_around, end_around + 1)]
 
     # Made array only with visible pages
@@ -44,10 +41,12 @@ def pagination(
         for page in range(1, len(visible))
     ]
     result_list.insert(0, visible[0])
+
     if not boundaries:
-        result_list.append("...")
         if result_list[0] != 1:
             result_list.insert(0, "...")
+        if current_around[-1] != total_pages:
+            result_list.append("...")
 
     result = " ".join(map(str, result_list))
 
@@ -56,4 +55,4 @@ def pagination(
 
 
 if __name__ == "__main__":
-    pagination(current_page=5, total_pages=300000000, boundaries=3, around=3)
+    pagination(current_page=9, total_pages=10, boundaries=0, around=1)
