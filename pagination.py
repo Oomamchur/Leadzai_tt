7dc1b10f764d607441d7e06a6c900751da7c49e5
Oleksandr Mamchur <irex1988@gmail.com>
2024-04-22 17:54:45 +0300

    fixed one-liners where it is convenient

--- pagination.py
+++ pagination.py
@@ -1,11 +1,9 @@
 def pagination(
     current_page: int, total_pages: int, boundaries: int, around: int
 ) -> str:
-    if not all(
-        isinstance(argument, int)
-        for argument in [current_page, total_pages, boundaries, around]
-    ):
-        raise ValueError("All values must be integers")
+    for argument in [current_page, total_pages, boundaries, around]:
+        if not isinstance(argument, int):
+            raise ValueError("All values must be integers")
     if current_page < 1 or total_pages < 1:
         raise ValueError("current_page & total_pages must be greater that 0")
     if around < 0 or boundaries < 0:
@@ -13,34 +11,29 @@ def pagination(
 
     # Made boundaries
     if boundaries <= total_pages:
-        start_boundary = [page for page in range(1, boundaries + 1)]
-        end_boundary = [
-            total_pages - page for page in range(boundaries - 1, -1, -1)
-        ]
+        start_boundary = list(range(1, boundaries + 1))
+        end_boundary = list(range(total_pages - boundaries + 1, total_pages + 1))
     else:
-        start_boundary = end_boundary = [
-            page for page in range(1, total_pages + 1)
-        ]
+        start_boundary = end_boundary = list(range(1, total_pages + 1))
 
     # Add around pages for current page
-    start_around = 1 if (current_page - around) < 1 else current_page - around
-    end_around = (
-        total_pages
-        if current_page + around > total_pages
-        else current_page + around
-    )
-    current_around = [page for page in range(start_around, end_around + 1)]
+    start_around = current_page - around
+    if current_page - around < 1:
+        start_around = 1
+    end_around = current_page + around
+    if current_page + around > total_pages:
+        end_around = total_pages
+    current_around = list(range(start_around, end_around + 1))
 
     # Made array only with visible pages
     visible = sorted(set(start_boundary + current_around + end_boundary))
 
-    result_list = [
-        visible[page]
-        if visible[page] == visible[page - 1] + 1
-        else f"... {visible[page]}"
-        for page in range(1, len(visible))
-    ]
-    result_list.insert(0, visible[0])
+    result_list = [visible[0]]
+    for page in range(1, len(visible)):
+        if visible[page] == visible[page - 1] + 1:
+            result_list.append(visible[page])
+        else:
+            result_list.append(f"... {visible[page]}")
 
     if not boundaries:
         if result_list[0] != 1:
